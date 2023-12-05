daf7fc4241d3f523e916a578bc3c9b671f8a05dd
ST3tter <st3tter@gmail.com>
2023-12-05 22:39:00 +0100

    Day 4 part one done

--- lib/day04/day04.ex
+++ lib/day04/day04.ex
@@ -8,7 +8,11 @@ defmodule AoC23.Day04 do
 
   defp part1 do
     get_input()
-    -1
+    |> Enum.map(fn x ->
+      find_winning_numbers(x)
+      |> pow()
+    end)
+    |> Enum.sum()
   end
 
   defp part2 do
@@ -17,7 +21,29 @@ defmodule AoC23.Day04 do
   end
 
   defp get_input do
-    File.read!("lib/day04/test_input.txt")
+    File.read!("lib/day04/input.txt")
     |> String.split("\n")
+    |> Enum.map(fn x ->
+      String.replace(x, ~r/Card\s{1,3}[\d]{1,3}:\s{1,3}/, "")
+      |> String.split(" | ")
+      |> Enum.map(fn y -> String.split(y, ~r/\s{1,2}/) end)
+    end)
+  end
+
+  defp find_winning_numbers([winners, numbers]) do
+    Enum.reduce(numbers, 0, fn x, acc ->
+      if Enum.member?(winners, x) do
+        acc + 1
+      else
+        acc
+      end
+    end)
+  end
+
+  defp pow(n) do
+    cond do
+      n == 0 -> 0
+      true -> 2 ** (n - 1)
+    end
   end
 end
